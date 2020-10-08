80f2f6168a88e4efd61204494b2f30552ac7f691
Hong-JunHyeok <edb1631@naver.com>
2020-10-08 10:07:09 +0900

    text부분 추가&디자인 개선

--- src/components/CanvasWrapper.js
+++ src/components/CanvasWrapper.js
@@ -4,9 +4,20 @@ import "./CanvasWrapper.scss";
 function CanvasWrapper() {
     return (
         <div className="CanvasWrapper">
-            <div className="CanvasWrapper_text">여긴 텍스트</div>
-            <div className="CanvasWrapper_draw">여긴 그림</div>
+            <div className="CanvasWrapper_text_container">
+                <textarea
+                    name=""
+                    className="CanvasWrapper_text"
+                    placeholder="여기에 텍스트를 입력하세요"
+                    cols="120"
+                    rows="10"
+                ></textarea>
+            </div>
+            <div className="CanvasWrapper_draw_container">
+                <canvas></canvas>
+            </div>
         </div>
     );
 }
+
 export default CanvasWrapper;
